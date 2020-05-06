5a9050f19915ff7c9019f95af7a3f3e147cdfc75
xrxoxcxox <watanuki@ju-zen.com>
2020-05-07 00:49:23 +0900

    Add posts list

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
 import Head from 'next/head'
+import { getSortedPostsData } from '../lib/posts'
 import Layout, { siteTitle } from '../components/layout'
+import Link from 'next/link'
 import utilStyles from '../styles/utils.module.css'
 
-import Link from 'next/link'
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -16,6 +25,20 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
