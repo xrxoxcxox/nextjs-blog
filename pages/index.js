6702946e56c48af590ae22e08d34560d9679bbde
xrxoxcxox <watanuki@ju-zen.com>
2020-05-07 01:45:53 +0900

    Add links to each post page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import { getSortedPostsData } from '../lib/posts'
 import Layout, { siteTitle } from '../components/layout'
-import Link from 'next/link'
 import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+import Date from '../components/date'
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
@@ -31,11 +32,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
